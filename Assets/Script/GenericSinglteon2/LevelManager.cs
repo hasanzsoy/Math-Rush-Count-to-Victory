e1b6560fc778e494bc11885c40d0a09d4e398d65
Hasan ÖZSOY <hasanzsoy@outlook.com>
2026-07-22 11:21:05 +0300

    Gereksiz Dosyalar Silindi

--- Assets/Script/GenericSinglteon2/LevelManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LevelManager : GenericSingelton<LevelManager>
-{
-    public void LoadLevel(int levelIndex) 
-    {       
-        Debug.Log($"Loading Level {levelIndex}");
-    }
-}
